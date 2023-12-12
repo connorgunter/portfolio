535a79c5df2af29195fdb817b45ac83ea4ecd982
Connor Gunter <bigc5674@gmail.com>
2023-12-12 13:36:00 -0500

    styled project buttons and footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,23 @@
+import "./Footer.css";
 function Footer(props) {
-    return <h1>Footer</h1>;
-  }
-  
-  export default Footer;
-  
+  return (
+    <div className="footer-container">
+      <p className="footer-text">Contact Me</p>
+      <div className="github-logo-container">
+        <a
+          href="https://github.com/connorgunter"
+          rel="noopener noreferrer"
+          target="_blank"
+          className="github-link"
+        >
+          <img
+            className="github-logo"
+            src="../../public/images/GitHub-logo.png"
+          ></img>
+        </a>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,40 +1,44 @@
 import { useState, useEffect } from "react";
+import "../css/Projects.css";
 
-function Projects(props) {
-  // create state to hold projects
+function Projects() {
   const [projects, setProjects] = useState(null);
 
-  //create function to make api call
   const getProjectsData = async () => {
-
-		//make api call and get response
     const response = await fetch("./projects.json");
 
-		// turn response into javascript object
     const data = await response.json();
 
-		// set the projects state to the data
     setProjects(data);
-
   };
 
-  // make an initial call for the data inside a useEffect, so it only happens once on component load
-  useEffect(() => {getProjectsData()}, []);
+  useEffect(() => {
+    getProjectsData();
+  }, []);
 
-  // define a function that will return the JSX needed once we get the data
   const loaded = () => {
-    return projects.map((project, idx) => (
-      <div key={idx}>
-        <h1>{project.name}</h1>
-        <img src={project.image} />
-        <a href={project.git}>
-          <button>Github</button>
-        </a>
-        <a href={project.live}>
-          <button>live site</button>
-        </a>
+    return (
+      <div>
+        <h1>My Projects</h1>
+        <div className="projects-container">
+          {projects.map((project, idx) => (
+            <div className="project-card" key={idx}>
+              <h1>{project.name}</h1>
+              <img src={project.image} />
+              <div className="buttons">
+                <a href={project.git}>
+                  <button className="slide">Github</button>
+                </a>
+                <a href={project.live}>
+                  <button className="slide">Link!</button>
+                </a>
+              </div>
+            </div>
+          ))}
+          ;
+        </div>
       </div>
-    ));
+    );
   };
 
   return projects ? loaded() : <h1>Loading...</h1>;
