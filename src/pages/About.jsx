1ac8a2b6e4e6ff8ec9daff0cf15da7ce336a8413
Connor Gunter <bigc5674@gmail.com>
2024-01-25 10:40:18 -0500

    updated projects

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,26 +2,19 @@ import { useState, useEffect } from "react";
 import "../css/About.css"
 
 function About() {
-  // create state to hold about data
   const [about, setAbout] = useState(null);
 
-  // create function to make api call
   const getAboutData = async () => {
 
-		// make api call and get response
     const response = await fetch("./skills.json");
 
-		// turn response into javascript object
     const data = await response.json();
 
-		// set the about state to the data
     setAbout(data);
   };
 
-  // make an initial call for the data inside a useEffect, so it only happens once on component load
   useEffect(() => { getAboutData() } , []);
 
-  // define a function that will return the JSX needed once we get the data
   const loaded = () => (
     <div className="about-container">
       <h2 className="about-name">{about.name}</h2>
@@ -30,7 +23,6 @@ function About() {
     </div>
   );
 
-  // if data arrives return the result of loaded, if not, an h1 that says loading
   return (
     <div id="about">
     {about ? loaded() : <h1>Loading...</h1>};
