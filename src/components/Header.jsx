c4f6ba96ff5de92ab5fb9a3a1cc78abeb9d0efed
Connor Gunter <bigc5674@gmail.com>
2024-01-29 12:19:14 -0500

    resume button added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -17,6 +17,12 @@ function Header() {
         <a href="#projects">
           <div className="link">PROJECTS</div>
         </a>
+        <div className="resume-container">
+        <a rel="noreferrer" target="_blank" href="https://drive.google.com/file/d/1pombqrcirYJQMgBH3dexe6YZwDQ5BL4C/view?usp=sharing">
+        <img className="resume" src="../../images/resume.png"/>
+        <span className="popup">Resume</span>
+        </a>
+        </div>
       </nav>
     </header>
   );
