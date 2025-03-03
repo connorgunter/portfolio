b7bb4e32d7e3b3f72d303be424e953977f60e025
Connor Gunter <bigc5674@gmail.com>
2025-03-03 15:31:20 -0500

    changed photo, code cleanup

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -29,7 +29,7 @@ function Home() {
       <div className="flex flex-col md:flex-row items-center justify-evenly">
         <img
           className="shadow-[0_4px_8px_rgba(0,0,0,0.1)] w-[250px] h-auto mt-5 md:mt-0 rounded-[50%] border-[3px] border-solid border-[black] mb-5 md:mb-0"
-          src="../../images/maine.png"
+          src="../../images/headshot.png"
         />
         <p className="text-[#333] text-lg leading-[1.6] max-w-[600px] my-5 text-left">
           I grew up in a charming town in southern Maine, surrounded by a
@@ -43,35 +43,6 @@ function Home() {
           boundaries of what is possible.
         </p>
       </div>
-      {/* <div className="a-c">
-        <div className="about-container">
-          <button className="accordion">My Journey</button>
-          <div className="panel">
-            <p>
-              With over six years of experience in the food service industry, my
-              initial career aspirations were rooted in culinary arts during my
-              high school years. However, my longstanding passion for technology
-              and computers eventually steered my career path in a different
-              direction. At the age of 16, I acquired my first desktop computer,
-              dedicating countless hours to gaming and enhancing its performance
-              for an optimal gaming experience. This deep-seated interest in
-              technology was further nurtured when my brother introduced me to
-              the world of programming. Embracing this new-found passion, I
-              decided to enroll in a bootcamp through General Assembly, a
-              decision that has proven to be transformative and pivotal in my
-              career journey.
-            </p>
-          </div>
-          <button className="accordion">Goals</button>
-          <div className="panel">
-            <p>Panel 2</p>
-          </div>
-          {/* <button className="accordion"></button>
-          <div className="panel">
-            <p>Panel 3</p>
-          </div> */}
-      {/* </div> */}
-      {/* {</div> */}
     </div>
   );
 }
