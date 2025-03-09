8d45f0ca8ac4591a02c49d795db352e12501da75
Connor Gunter <bigc5674@gmail.com>
2025-03-09 19:04:23 -0400

    removed useless code

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,22 +1,5 @@
 import "../css/About.css";
-import { useEffect } from "react";
 function Home() {
-  useEffect(() => {
-    const acc = document.getElementsByClassName("accordion");
-    let i;
-
-    for (i = 0; i < acc.length; i++) {
-      acc[i].addEventListener("click", function () {
-        this.classList.toggle("active");
-        var panel = this.nextElementSibling;
-        if (panel.style.maxHeight) {
-          panel.style.maxHeight = null;
-        } else {
-          panel.style.maxHeight = panel.scrollHeight + "px";
-        }
-      });
-    }
-  }, []);
   return (
     <div id="about" className="text-center p-5">
       <br />
